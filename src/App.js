e111d5110a9dab79beb86c3eed03afd744422283
Bulbul89 <bulbul.ei2236@gmail.com>
2023-06-04 12:35:35 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import CarForm from "./components/CarForm";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
+import CarList from "./components/CarsList";
+
 function App () {
     return <div>
-        hi
+        <CarForm />
+        <CarList />
+        <CarSearch />
+        <CarValue />
     </div>
 }
 export default App;
---
+++ src/components/CarsList.js
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+
+function CarList () {
+    const cars = useSelector( (state) => {
+            return state.cars.cars
+    })
+
+    console.log(cars)
+    return <div>
+        CarList
+    </div>
+}
+export default CarList;
